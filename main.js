2c64351b8f71444166eb303886374cbd299bfee3
syn <vicdemilly@gmail.com>
2019-04-11 09:57:15 +0200

    Autocorrection et ajout de commentaires

---
+++ main.js
@@ -0,0 +1,5 @@
+function changeImage(){
+  document.getElementById('image1').src='assets/img/image2.jpg'
+}
+//.src permet d'idendiquer le chemin d'une autre image
+//La fonction que j'ai nommé changeImage se déclenche grâce au onmouseover dans le HTML
